73d4ac44d8a532b85370fbcd977486971ce6beaf
Julia132 <uka132@mail.ru>
2019-07-29 23:49:41 +0400

    Update brain_dataset_git.py

--- brain_dataset_git.py
+++ brain_dataset_git.py
@@ -16,64 +16,70 @@ onlyfiles_out = [f for f in listdir(mypath_out) if isfile(join(mypath_out, f))]
 img = np.empty(len(onlyfiles_out), dtype=object)
 
 with open('data_set_2.csv', 'w', newline='') as f:
-    wr = csv.writer(f, delimiter=',', quotechar=',',
+        wr = csv.writer(f, delimiter=',', quotechar=',',
                     quoting=csv.QUOTE_MINIMAL)
-    for n in range(0, len(onlyfiles_out)):
+        for n in range(0, len(onlyfiles_out)):
 
-        img[n] = cv2.imread(join(mypath_in, onlyfiles_out[n]))
-        gray = cv2.cvtColor(img[n], cv2.COLOR_BGR2GRAY)
-        newImg = cv2.resize(gray, (512, 512))
-        R = np.mean(newImg)
-        std = np.std(newImg)
+            img[n] = cv2.imread(join(mypath_in, onlyfiles_out[n]))
+            gray = cv2.cvtColor(img[n], cv2.COLOR_BGR2GRAY)
+            newImg = cv2.resize(gray, (512, 512))
+            R = np.mean(newImg)
+            std = np.std(newImg)
 
-        img[n] = cv2.imread(join(mypath_out, onlyfiles_out[n]))
-        gray = cv2.cvtColor(img[n], cv2.COLOR_BGR2GRAY)
+            img[n] = cv2.imread(join(mypath_out, onlyfiles_out[n]))
+            gray = cv2.cvtColor(img[n], cv2.COLOR_BGR2GRAY)
 
-        closing = cv2.resize(gray, (512, 512))
+            closing = cv2.resize(gray, (512, 512))
 
-        height, width = closing.shape
-        ret, threshold_image = cv2.threshold(closing, 127, 255, 0)
+            height, width = closing.shape
+            ret, threshold_image = cv2.threshold(closing, 127, 255, 0)
 
-        _, contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours) > 1:
-            print("WARNING! len(contours) == ", len(contours))
-            for contour in contours:
-                print(len(contour))
-        contours_cirles = []
+            _, contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 1:
+                print("WARNING! len(contours) == ", len(contours))
+                for contour in contours:
+                    print(len(contour))
+            hull = []
+            contours.sort(key=lambda contour: len(contour), reverse=True)
 
-        hull = []
-        area = []
-        contours.sort(key=lambda contour: len(contour))
-        i = len(contours) - 1
-        area = cv2.contourArea(contours[i]) #выходит за границы массива?
-        hull = cv2.convexHull(contours[i])
-        hull_area = cv2.contourArea(hull)
+            i = len(contours)-1
+            #contours.sort(key=cv2.contourArea, reverse=True)
+            #area = max(contours[i], key=cv2.contourArea)
 
-        _, radius = cv2.minEnclosingCircle(contours[i])
-        radius = int(radius)
+            (+_+) (-_-;) (?_?)  (~~>.<~~)
 
-        if 0 < area and 0 < radius:
+            #area = cv2.contourArea(contours[i])#выходит за границы массива?
+            #area = ((cv2.contourArea(contour), contour) for i in contours)
 
-            square = area / (width*height)
-            rh = radius / height
-            ans = []
-            for y in range(threshold_image.shape[0]):
-                for x in range(threshold_image.shape[1]):
-                    if threshold_image[y, x] > 0:
-                        ans.append((y, x))
-            xx = [x for (y, x) in ans]
-            yy = [y for (y, x) in ans]
-            x = (max(xx) - min(xx)) / threshold_image.shape[1]
-            y = (max(yy) - min(yy)) / threshold_image.shape[0]
-            ellipse = cv2.minAreaRect(contours[i])
-            (center, axes, orientation) = ellipse
-            majoraxis_length = max(axes)
-            minoraxis_length = min(axes)
-            perimeter = cv2.arcLength(contours[i], True)
-            parametr_e = (lambda: (math.sqrt(1 - (minoraxis_length / majoraxis_length) ** 2)))()
-            parametr_c = (lambda: (4 * pi * area / perimeter ** 2))()
-            parametr_s = (lambda: (float(area) /hull_area))()
 
-            wr.writerows([(parametr_s, parametr_e, parametr_c, hull_area, area, perimeter, square,
+            hull = cv2.convexHull(contours[i])
+            hull_area = cv2.contourArea(hull)
+
+            _, radius = cv2.minEnclosingCircle(contours[i])
+            radius = int(radius)
+
+            if 0 < radius:
+
+                square = area / (width*height)
+                rh = radius / height
+                ans = []
+                for y in range(threshold_image.shape[0]):
+                    for x in range(threshold_image.shape[1]):
+                        if threshold_image[y, x] > 0:
+                            ans.append((y, x))
+                xx = [x for (y, x) in ans]
+                yy = [y for (y, x) in ans]
+                x = (max(xx) - min(xx)) / threshold_image.shape[1]
+                y = (max(yy) - min(yy)) / threshold_image.shape[0]
+                ellipse = cv2.minAreaRect(contours[i])
+                (center, axes, orientation) = ellipse
+                majoraxis_length = max(axes)
+                minoraxis_length = min(axes)
+                perimeter = cv2.arcLength(contours[i], True)
+                parametr_e = (lambda: (math.sqrt(1 - (minoraxis_length / majoraxis_length) ** 2)))()
+                parametr_c = (lambda: (4 * pi * area / perimeter ** 2))()
+                parametr_s = (lambda: ((area) /hull_area))()
+
+                wr.writerows([(parametr_s, parametr_e, parametr_c, hull_area, area, perimeter, square,
                            rh, x, y, R, std)])
 
