15b0339e9042ac41fd689c0638a4dc0cae3a5910
Julia132 <uka132@mail.ru>
2019-08-04 03:05:41 +0400

    Create START.py

---
+++ START.py
@@ -0,0 +1,11 @@
+from brain_main_git import main
+main()
+from brain_selection_git import selection
+selection()
+from characteristic_git import characteristic
+mask5 = main()
+characteristic(mask5)
+from brain_dataset_git import dataset
+dataset()
+from csv_git import csv_model
+csv_model()
