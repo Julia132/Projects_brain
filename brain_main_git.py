a666ec4cf6ad4de82814f70345487d98f10b75bb
Julia132 <uka132@mail.ru>
2019-08-19 23:30:08 +0400

    Update brain_main_git.py

--- brain_main_git.py
+++ brain_main_git.py
@@ -9,6 +9,54 @@ from math import pi  # для константы pi
 from imblearn.metrics import sensitivity_specificity_support
 
 
+positive_number = 98
+
+
+def selection(contours, img_name, shape, mypath_late, mypath_no):
+    hull = []
+    # for i in range(0, len(contours)):
+    #     hull.append(cv2.convexHull(contours[i], False))
+    #     cv2.drawContours(drawing, contours, i, 255, 1, 51, hierarchy)
+    #     closing = cv2.drawContours(drawing, hull, i, 100, 1, 8)
+    mask = np.zeros(shape, np.uint8)
+    for i in range(0, len(contours)):
+        area = cv2.contourArea(contours[i])
+        hull = cv2.convexHull(contours[i])
+        hull_area = cv2.contourArea(hull)
+        _, radius = cv2.minEnclosingCircle(contours[i])
+        radius = int(radius)
+        if 300 < area < 10000 and 10 < radius < 100:
+            ellipse = cv2.fitEllipse(contours[i])
+            (center, axes, orientation) = ellipse
+            majoraxis_length = max(axes)
+            minoraxis_length = min(axes)
+            eccentricity = (np.sqrt(1 - (minoraxis_length / majoraxis_length) ** 2))
+            perimeter = cv2.arcLength(contours[i], True)
+            circularity = 4 * pi * (area / (perimeter * perimeter))
+            solidity = float(area) / hull_area
+            if 0.2 < circularity < 1.2 and 0.1 < eccentricity < 0.9 and solidity > 0.7:
+                cv2.drawContours(mask, contours, i, 255, -1)
+    if np.mean(mask) < 1.0e-6:
+        cv2.imwrite(join(mypath_no, img_name), mask)
+        cv2.imwrite(join(mypath_late, img_name), mask)
+    else:
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        Max = 0
+        pos = 0
+        mask2 = np.zeros(mask.shape, np.uint8)
+        for idx in range(0, len(contours)):
+            area = cv2.contourArea(contours[idx])
+            if area > Max:
+                Max = area
+                pos = idx
+        Contour = np.array(contours[pos])
+        cv2.drawContours(mask2, [Contour], 0, 255, -1)
+        mask3 = cv2.medianBlur(mask2, 9)
+        mask4 = cv2.medianBlur(mask3, 9)
+        mask5 = cv2.medianBlur(mask4, 9)
+        cv2.imwrite(join(mypath_late, img_name), mask5)
+        return mask5
+
 def main():
     mypath_in = 'C:/Users/inet/Desktop/part_start'  #больные и здоровые в серых тонах
     mypath_out = 'C:/Users/inet/Desktop/part_finish_0'    #результат обработки больных и здоровых вручную
@@ -36,51 +84,8 @@ def main():
         closing = cv2.morphologyEx(erode, cv2.MORPH_CLOSE, kernel, 5)
 
         _, contours, hierarchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        drawing = np.zeros(closing.shape, np.uint8)
-        def selection(contours):
-            hull = []
-            for i in range(0, len(contours)):
-                hull.append(cv2.convexHull(contours[i], False))
-                cv2.drawContours(drawing, contours, i, 255, 1, 51, hierarchy)
-                closing = cv2.drawContours(drawing, hull, i, 100, 1, 8)
-            mask = np.zeros(closing.shape, np.uint8)
-            for i in range(0, len(contours)):
-                area = cv2.contourArea(contours[i])
-                hull = cv2.convexHull(contours[i])
-                hull_area = cv2.contourArea(hull)
-                _, radius = cv2.minEnclosingCircle(contours[i])
-                radius = int(radius)
-                if 300 < area < 10000 and 10 < radius < 100:
-                        ellipse = cv2.fitEllipse(contours[i])
-                        (center, axes, orientation) = ellipse
-                        majoraxis_length = max(axes)
-                        minoraxis_length = min(axes)
-                        eccentricity = (np.sqrt(1 - (minoraxis_length / majoraxis_length) ** 2))
-                        perimeter = cv2.arcLength(contours[i], True)
-                        circularity = 4 * pi * (area / (perimeter * perimeter))
-                        solidity = float(area) / hull_area
-                        if 0.2 < circularity < 1.2 and 0.1 < eccentricity < 0.9 and solidity > 0.7:
-                            cv2.drawContours(mask, contours, i, 255, -1)
-            if int(np.mean(mask)) == 0:
-                cv2.imwrite(join(mypath_no, onlyfiles[n]), mask)
-            if int(np.mean(mask)) >= 1:
-                _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                Max = 0
-                pos = 0
-                mask2 = np.zeros(mask.shape, np.uint8)
-                for idx in range(0, len(contours)):
-                    area = cv2.contourArea(contours[idx])
-                    if area > Max:
-                        Max = area
-                        pos = idx
-                Contour = np.array(contours[pos])
-                cv2.drawContours(mask2, [Contour], 0, 255, -1)
-                mask3 = cv2.medianBlur(mask2, 9)
-                mask4 = cv2.medianBlur(mask3, 9)
-                mask5 = cv2.medianBlur(mask4, 9)
-                cv2.imwrite(join(mypath_late, onlyfiles[n]), mask5)
-            return mask5
-    mask5 = selection(contours)
+        # drawing = np.zeros(closing.shape, np.uint8)
+        selection(contours, onlyfiles[n], closing.shape, mypath_late, mypath_no)
     list_precision = []
     list_recall = []
     list_fscore = []
@@ -89,7 +94,7 @@ def main():
     start_img = np.empty(len(onlyfiles_out), dtype=object)
     final_img = np.empty(len(onlyfiles_out), dtype=object)
     onlyfiles_late = [f for f in listdir(mypath_late) if isfile(join(mypath_late, f))]
-    for n in range(0, len(onlyfiles_late)):
+    for n in range(0, positive_number):
         start_img[n] = cv2.imread(join(mypath_late, onlyfiles_late[n]), cv2.IMREAD_GRAYSCALE) #ошибка в количестве файлов
         final_img[n] = cv2.imread(join(mypath_out, onlyfiles_late[n]), cv2.IMREAD_GRAYSCALE)
         gray_s = cv2.resize(start_img[n], (512, 512))
@@ -118,7 +123,8 @@ def main():
     print('binary fscore value', Avg_fscore)
     print('binary specificity value', Avg_specificity)
     print('binary accuracy value', Agv_accuracy)
-    return mask5
-mask5 = main()
+
+
+
 if __name__=="__main__":
     main()
