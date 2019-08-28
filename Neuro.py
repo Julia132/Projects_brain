72e31c0290868af73806a0f445d1acf945839861
Julia132 <uka132@mail.ru>
2019-08-28 15:56:53 +0400

    Update Neuro.py

--- Neuro.py
+++ Neuro.py
@@ -1,41 +1,141 @@
-from numpy import exp, array, random, dot
-import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 import numpy as np
-import csv
-class NeuralNetwork():
-    training_set_inputs = pd.read_csv("C:/Users/inet/Documents/GitHub/Projects_brain/data_set_2.csv", sep=',',
-                                      encoding='latin1',
-                                      dayfirst=True,
-                                      index_col=None, header=None)
-    def __init__(self):
-        self.synaptic_weights = self.training_set_inputs
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+#np.random.seed(1000)
+#Instantiate an empty model
 
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in range(number_of_training_iterations):
-            output = self.think(training_set_inputs)
-            error = training_set_outputs - output
-            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
-            self.synaptic_weights += adjustment
+data = []
+labels = []
 
-    def think(self, inputs):
+# берём пути к изображениям и рандомно перемешиваем
+imagePaths = sorted(list(paths.list_images("C:/Users/inet/Desktop/part_start")))
+random.seed(200)
+random.shuffle(imagePaths)
 
-        return self.__sigmoid(dot(inputs, self.synaptic_weights))
-if __name__ == "__main__":
-    neural_network = NeuralNetwork()
+# цикл по изображениям
+for imagePath in imagePaths:
+    # загружаем изображение, меняем размер на 32x32 пикселей (без учёта
+    # соотношения сторон), сглаживаем его в 32x32x3=3072 пикселей и
+    # добавляем в список
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (128, 128)).flatten()
+    data.append(image)
 
-    print("Random starting synaptic weights: ")
-    print(neural_network.synaptic_weights)
-    training_set_inputs = pd.read_csv("C:/Users/inet/Documents/GitHub/Projects_brain/data_set_2.csv", sep=',', encoding='latin1',
-                          dayfirst=True,
-                          index_col=None, header=None)
-    training_set_outputs = pd.read_csv("C:/Users/inet/Documents/GitHub/Projects_brain/out.csv", sep=',', encoding='latin1',
-                          dayfirst=True,
-                          index_col=None, header=None).T
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
-    print("New synaptic weights after training: ")
-    print(neural_network.synaptic_weights)
+    # извлекаем метку класса из пути к изображению и обновляем
+    # список меток
+    label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
+data = np.array(data, dtype="float") / 255.0
+np.moveaxis(data,1,-1)
+labels = np.array(labels)
+(trainX, testX, trainY, testY) = train_test_split(data,
+    labels, test_size=0.25, random_state=200)
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+model = Sequential()
+
+
+
+# 1st Convolutional Layer
+model.add(Conv2D(filters=96, input_shape=(224,224,3), kernel_size=(11,11), strides=(4,4), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+# 2nd Convolutional Layer
+model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+# 3rd Convolutional Layer
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+
+# 4th Convolutional Layer
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+
+# 5th Convolutional Layer
+model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+# Passing it to a Fully Connected layer
+model.add(Flatten())
+# 1st Fully Connected Layer
+model.add(Dense(4096, input_shape=(224*224*3,)))
+model.add(Activation('relu'))
+# Add Dropout to prevent overfitting
+model.add(Dropout(0.4))
+
+# 2nd Fully Connected Layer
+model.add(Dense(4096))
+model.add(Activation('relu'))
+# Add Dropout
+model.add(Dropout(0.4))
+
+# 3rd Fully Connected Layer
+model.add(Dense(1000))
+model.add(Activation('relu'))
+# Add Dropout
+model.add(Dropout(0.4))
+
+# Output Layer
+model.add(Dense(17))
+model.add(Activation('softmax'))
+
+model.summary()
+
+# Compile the model
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+
+batch = np.expand_dims(image,axis=0)
+print(batch.shape) # (1, 28, 28)
+batch = np.expand_dimes(batch,axis=3)
+print(batch.shape) # (1, 28, 28,1)
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+                            predictions.argmax(axis=1), target_names=lb.classes_))
+
+INIT_LR = 0.01
+EPOCHS = 75
+H = model.fit(trainX, trainY, validation_data=(testX, testY),
+    epochs=EPOCHS, batch_size=32, channels=1, rows =128, cols =128)
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+    predictions.argmax(axis=1), target_names=lb.classes_))
+# строим графики потерь и точности
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["acc"], label="train_acc")
+plt.plot(N, H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy (Simple NN)")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+
 
