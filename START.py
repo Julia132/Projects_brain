e3926fb3505117b3fac5690693301dabc798fb3c
Julia132 <uka132@mail.ru>
2020-02-06 22:33:56 +0400

    code example and images example added

--- START.py
+++ START.py
@@ -1,6 +1,22 @@
+"""""
+This module activates the program
+
+############################
+
+The program is comprised of three basic modules:
+
+brain_main_git - multiple stage processing and analysis of inner contours
+
+brain_dataset_git - counting various geometric features in the contours
+
+csv_git - classification geometric features from .csv, analysis of job
+"""""
+
 from brain_main_git import main
 main()
+
 from brain_dataset_git import dataset
 dataset()
+
 from csv_git import csv_model
 csv_model()
