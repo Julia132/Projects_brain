3c6fcf3889c78d929923baa644952d44e49ec2b2
Julia132 <uka132@mail.ru>
2020-02-07 00:53:19 +0400

    refactoring

--- brain_dataset_git.py
+++ brain_dataset_git.py
@@ -3,23 +3,35 @@ import numpy as np
 import csv
 import math
 
-from os import listdir  # список файлов в папке
-from os.path import isfile, join  # методы проверки файла и соединения с файлом
-from math import pi  # для константы pi
+from os import listdir
+from os.path import isfile, join
+from math import pi
 
 #from scipy.stats import kurtosis
 #from scipy.stats import skew
 
+
 def dataset():
-    mypath_in = 'C:/Users/inet/Desktop/part_start'
-    mypath_late = 'C:/Users/inet/Desktop/part_start_late'
+
+    """""
+
+    This function count compute 12 geometric features,
+    note result in  file data_set_geometric_features.csv
+
+    """""
+
+
+    mypath_in = 'part_start'
+    mypath_late = 'part_start_late'
     onlyfiles = [f for f in listdir(mypath_in) if isfile(join(mypath_in, f))]
     onlyfiles_out = [f for f in listdir(mypath_late) if isfile(join(mypath_late, f))]
     img = np.empty(len(onlyfiles_out), dtype=object)
 
-    with open('data_set_new.csv', 'w', newline='') as f:
+    with open('data_set_geometric_features.csv', 'w', newline='') as f:
+
             wr = csv.writer(f, delimiter=',', quotechar=',',
                         quoting=csv.QUOTE_MINIMAL)
+
             for n in range(0, len(onlyfiles)):
 
                 img[n] = cv2.imread(join(mypath_in, onlyfiles[n]))
@@ -37,13 +49,18 @@ def dataset():
                 ret, threshold_image = cv2.threshold(closing, 127, 255, 0)
 
                 _, contours, hierarchy = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
                 if len(contours) > 1:
                     print("WARNING! len(contours) == ", len(contours))
+
                     for contour in contours:
                         print(len(contour))
+
                 contours.sort(key=lambda contour: len(contour), reverse=True)
+
                 if len(contours) == 0:
                     continue
+
                 i = 0
                 area = cv2.contourArea(contours[i])
                 hull = cv2.convexHull(contours[i])
@@ -57,10 +74,14 @@ def dataset():
                     square = area / (width*height)
                     rh = radius / height
                     ans = []
+
                     for y in range(threshold_image.shape[0]):
+
                         for x in range(threshold_image.shape[1]):
+
                             if threshold_image[y, x] > 0:
                                 ans.append((y, x))
+
                     xx = [x for (y, x) in ans]
                     yy = [y for (y, x) in ans]
                     x = (max(xx) - min(xx)) / threshold_image.shape[1]
@@ -76,8 +97,10 @@ def dataset():
 
                     wr.writerows([(parametr_s, parametr_e, parametr_c, hull_area, area, perimeter, square,
                                rh, x, y, R, std)])
+
                 data_model = parametr_s, parametr_e, parametr_c, hull_area, area, perimeter, square, rh, x, y, R, std
-                print(data_model)
+
+
     return data_model
 
 
