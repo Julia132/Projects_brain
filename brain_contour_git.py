9d2b7ea94e0d977cd3389a49069f24bcda7f0f26
Julia132 <uka132@mail.ru>
2020-02-07 00:48:56 +0400

    At this time, we don't use this module

--- brain_contour_git.py
+++ brain_contour_git.py
@@ -7,7 +7,6 @@ from os.path import isfile, join
 
 def contour():
 
-
     mypath_late = 'part_start'
     mypath_contour = 'contour'
     mypath_out = 'part_finish'
@@ -16,14 +15,19 @@ def contour():
     onlyfiles_out = [f for f in listdir(mypath_out) if isfile(join(mypath_out, f))]
     img = np.empty(len(onlyfiles_no), dtype=object)
     img_f = np.empty(len(onlyfiles_out), dtype=object)
+
     for n in range(0, len(onlyfiles_no)):
+
         img[n] = cv2.imread(join(mypath_late, onlyfiles_no[n]))
         edges = cv2.Canny(img[n], 0, 255)
         cv2.imwrite(join(mypath_contour, onlyfiles_no[n]), edges)
+
     for n in range(0, len(onlyfiles_out)):
+
         img_f[n] = cv2.imread(join(mypath_out, onlyfiles_no[n]))
         edges = cv2.Canny(img[n], 0, 255)
         cv2.imwrite(join(mypath_contour, onlyfiles_no[n]), edges)
+
     return edges
 
 
