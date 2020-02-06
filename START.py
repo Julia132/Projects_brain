6b77c787d16588c3054556394be9b52f7012351a
Julia132 <uka132@mail.ru>
2020-02-06 22:35:38 +0400

    modules description added

--- START.py
+++ START.py
@@ -10,6 +10,7 @@ brain_main_git - multiple stage processing and analysis of inner contours
 brain_dataset_git - counting various geometric features in the contours
 
 csv_git - classification geometric features from .csv, analysis of job
+
 """""
 
 from brain_main_git import main
